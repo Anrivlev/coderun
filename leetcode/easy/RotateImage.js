d050c00e152c8df83fc3b0df60c3d5024ac45ade
anrivlev <ivlev@itgeo.ru>
2026-08-13 15:06:18 +0300

    RotateImage.js

---
+++ leetcode/easy/RotateImage.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  const n = matrix.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      const temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+
+  for (const row of matrix) {
+    row.reverse();
+  }
+
+  return matrix;
+};
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+console.log(rotate(matrix));
