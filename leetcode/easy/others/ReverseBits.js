e238ac367e80c34646b04d541a38df827c23f594
anrivlev <ivlev@itgeo.ru>
2026-08-14 17:57:53 +0300

    reversebits

---
+++ leetcode/easy/others/ReverseBits.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var reverseBits = function (n) {
+  let result = 0;
+
+  for (let i = 0; i < 32; i++) {
+    result = (result << 1) | (n & 1);
+  }
+
+  return result;
+};
+
+console.log(reverseBits(2147483644));
