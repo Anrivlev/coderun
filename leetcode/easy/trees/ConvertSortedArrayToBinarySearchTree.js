bfeeb638d733c909540756601c38f51947e35327
anrivlev <ivlev@itgeo.ru>
2026-08-14 10:41:51 +0300

    ConvertSortedArrayToBinarySearchTree.js

--- leetcode/easy/trees/ConvertSortedArrayToBinarySearchTree.js
+++ leetcode/easy/trees/ConvertSortedArrayToBinarySearchTree.js
@@ -10,6 +10,21 @@
  * @param {number[]} nums
  * @return {TreeNode}
  */
-var sortedArrayToBST = function(nums) {
-    
+var sortedArrayToBST = function (nums) {
+  return buildNode(nums, 0, nums.length);
 };
+
+function buildNode(nums, leftIndex, rightIndex) {
+  if (nums.length === 0) return null;
+  if (leftIndex === rightIndex) return null;
+  const mid = Math.floor((leftIndex + rightIndex) / 2);
+
+  const val = nums[mid];
+  const left = buildNode(nums, leftIndex, mid);
+  const right = buildNode(nums, mid + 1, rightIndex);
+
+  return { val, left, right };
+}
+
+const nums = [-10, -3, 0, 5, 9];
+console.log(sortedArrayToBST(nums));
