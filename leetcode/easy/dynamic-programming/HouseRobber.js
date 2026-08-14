f5385d1a9cacd7065c277dd379cfb20759f20891
anrivlev <ivlev@itgeo.ru>
2026-08-14 14:58:33 +0300

    HouseRobber.js

--- leetcode/easy/dynamic-programming/HouseRobber.js
+++ leetcode/easy/dynamic-programming/HouseRobber.js
@@ -2,6 +2,33 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
-    
+var rob = function (nums) {
+  if (nums.length === 0) return 0;
+  let bestUpToThis = nums[0];
+  let bestWithoutPrev = 0;
+  let hasStolenFromPrev = true;
+  let maximum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (!hasStolenFromPrev) {
+      bestWithoutPrev = bestUpToThis;
+      bestUpToThis += nums[i];
+      hasStolenFromPrev = true;
+    } else {
+      const withoutPrev = bestWithoutPrev + nums[i];
+      if (withoutPrev > bestUpToThis) {
+        bestWithoutPrev = bestUpToThis;
+        bestUpToThis = withoutPrev;
+        hasStolenFromPrev = true;
+      } else {
+        hasStolenFromPrev = false;
+      }
+    }
+
+    maximum = Math.max(maximum, bestUpToThis);
+  }
+
+  return maximum;
 };
+
+const nums = [1, 2, 3, 1];
+console.log(rob(nums));
