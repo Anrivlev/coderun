d523ed0ac89b26880d14e485107d75335e839dcc
anrivlev <ivlev@itgeo.ru>
2026-08-14 17:39:43 +0300

    HammingDistance.js

---
+++ leetcode/easy/others/HammingDistance.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function (x, y) {
+  let distance = 0;
+  for (let i = 0; i < 32; i++) {
+    const isEqual = (x & (2 ** i)) === (y & (2 ** i));
+    distance += isEqual ? 0 : 1;
+  }
+  return distance;
+};
+
+var hammingDistance = function (x, y) {
+  let xor = x ^ y;
+
+  let distance = 0;
+  while (xor > 0) {
+    xor = xor & (xor - 1);
+    distance++;
+  }
+
+  return distance;
+};
+
+console.log(hammingDistance(1, 4));
