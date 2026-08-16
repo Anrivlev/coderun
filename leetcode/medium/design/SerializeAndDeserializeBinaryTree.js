a842f56a667620bd572aeb883a0669968f345e05
Andrey Ivlev <warcraft1921@gmail.com>
2026-08-16 10:55:22 +0300

    начал SerializeAndDeserializeBinaryTree.js, не отдебажил

---
+++ leetcode/medium/design/SerializeAndDeserializeBinaryTree.js
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function (root) {
+  let layer = [root];
+  const nodesArray = [];
+  while (layer.length > 0) {
+    const nextLayer = [];
+    let isAnyNotNull = false;
+
+    for (let i = 0; i < layer.length; i++) {
+      const node = layer[i];
+      nodesArray.push(node?.val ?? null);
+      nextLayer.push(node);
+      if (node !== null) isAnyNotNull = true;
+    }
+
+    if (!isAnyNotNull) break;
+
+    layer = nextLayer;
+  }
+
+  return nodesArray.join(" ");
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function (data) {
+  const nodesArray = data.split(" ");
+  const length = nodesArray.length;
+  const depth = Math.floor(Math.log2(length));
+  const root = { val: nodesArray[0], left: null, right: null };
+
+  let prevLayer = [root];
+  for (let d = 1; d < depth; d++) {
+    const nextLayer = [];
+    for (let i = 0; i < prevLayer.length; i++) {
+      const node = prevLayer[i];
+      if (node === null) continue;
+      const leftVal = nodesArray[2 ** d + i * 2];
+      const rightVal = nodesArray[2 ** d + i * 2];
+      const left =
+        leftVal === null ? null : { val: leftVal, left: null, right: null };
+      const right =
+        rightVal === null ? null : { val: rightVal, left: null, right: null };
+      node.left = left;
+      node.right = right;
+      nextLayer.push(left, right);
+    }
+    prevLayer = nextLayer;
+  }
+
+  return root;
+};
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
