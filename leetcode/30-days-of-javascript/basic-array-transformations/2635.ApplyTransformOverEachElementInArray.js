1df5b9c63db87b46b777f8604701222dea5a19cf
Andrey Ivlev <warcraft1921@gmail.com>
2026-08-18 18:09:09 +0300

    basic-array-transformations

--- leetcode/30-days-of-javascript/basic-array-transformations/2635.ApplyTransformOverEachElementInArray.js
+++ leetcode/30-days-of-javascript/basic-array-transformations/2635.ApplyTransformOverEachElementInArray.js
@@ -3,6 +3,10 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
-    
+var map = function (arr, fn) {
+  const result = new Array(arr.length);
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = fn(arr[i], i);
+  }
+  return result;
 };
