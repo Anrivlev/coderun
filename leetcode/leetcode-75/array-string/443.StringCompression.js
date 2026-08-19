6dd8fb4275c136b1342bfa7c46f541ee8dce6e53
anrivlev <ivlev@itgeo.ru>
2026-08-19 14:02:06 +0300

    443.StringCompression.js

--- leetcode/leetcode-75/array-string/443.StringCompression.js
+++ leetcode/leetcode-75/array-string/443.StringCompression.js
@@ -2,7 +2,44 @@
  * @param {character[]} chars
  * @return {number}
  */
-var compress = function(chars) {
-    let s = '';
-    
+var compress = function (chars) {
+  let prevChar = chars[0];
+  let finalLength = 0;
+  let repeatedCharCount = 1;
+  for (let i = 1; i < chars.length; i++) {
+    const char = chars[i];
+    if (char !== prevChar) {
+      chars[finalLength] = prevChar;
+      finalLength++;
+      if (repeatedCharCount > 1) {
+        repeatedCharCount
+          .toString(10)
+          .split("")
+          .forEach((value) => {
+            chars[finalLength] = value;
+            finalLength++;
+          });
+      }
+      prevChar = char;
+      repeatedCharCount = 1;
+    } else repeatedCharCount += 1;
+  }
+  chars[finalLength] = prevChar;
+  finalLength++;
+  if (repeatedCharCount > 1) {
+    repeatedCharCount
+      .toString(10)
+      .split("")
+      .forEach((value) => {
+        chars[finalLength] = value;
+        finalLength++;
+      });
+  }
+  return finalLength;
 };
+
+console.log(compress(["a", "a", "b", "b", "c", "c", "c"]));
+console.log(compress(["a"]));
+console.log(
+  compress(["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"]),
+);
