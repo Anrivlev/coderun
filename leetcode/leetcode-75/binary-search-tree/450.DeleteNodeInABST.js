8894727253847baf3b73e9decbd7b8d294c9282a
anrivlev <ivlev@itgeo.ru>
2026-08-27 18:01:23 +0300

    Все еще неправильно

--- leetcode/leetcode-75/binary-search-tree/450.DeleteNodeInABST.js
+++ leetcode/leetcode-75/binary-search-tree/450.DeleteNodeInABST.js
@@ -62,3 +62,61 @@ function findLeftmost(node) {
   if (node.left === null) return node;
   return findLeftmost(node.left);
 }
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function (root, key) {
+  if (root === null) return null;
+  if (root.val === key) return null;
+  let current = root;
+  while (current) {
+    if (current.val < key) {
+      const { left } = current;
+      if (!left) return root;
+      if (left.val === key) {
+        if (!left.left && !left.right) {
+          current.left = null;
+        } else if (left.left && left.right) {
+          const rightmost = findRightmost(left.left, current);
+          left.val = rightmost.node.val;
+          deleteNodeFromParent(rightmost.node, rightmost.parent);
+        } else if (left.left) {
+          current.left = left.left;
+        } else {
+          current.left = left.right;
+        }
+        return root;
+      } else current = left;
+    } else {
+      const { right } = current;
+      if (!right) return root;
+      if (right.val === key) {
+        if (!right.left && !right.right) {
+          current.right = null;
+        } else if (right.left && right.right) {
+          const rightmost = findRightmost(right.left, current);
+          right.val = rightmost.node.val;
+          deleteNodeFromParent(rightmost.node, rightmost.parent);
+        } else if (right.left) {
+          current.right = right.left;
+        } else {
+          current.right = right.right;
+        }
+        return root;
+      } else current = right;
+    }
+  }
+
+  return root;
+};
