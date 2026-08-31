e5d40bbc094cb0300ca1d127a8f338cd8f1ccb98
anrivlev <ivlev@itgeo.ru>
2026-08-31 15:01:26 +0300

    2300.SuccessfulPairsOfSpellsAndPotions.js

--- leetcode/leetcode-75/binary-search/2300.SuccessfulPairsOfSpellsAndPotions.js
+++ leetcode/leetcode-75/binary-search/2300.SuccessfulPairsOfSpellsAndPotions.js
@@ -4,6 +4,29 @@
  * @param {number} success
  * @return {number[]}
  */
-var successfulPairs = function(spells, potions, success) {
-    
+var successfulPairs = function (spells, potions, success) {
+  const n = spells.length;
+  const m = potions.length;
+
+  potions.sort((a, b) => b - a); // desc sort
+
+  return spells.map((spell) => {
+    const target = success / spell;
+
+    let left = 0;
+    let right = potions.length;
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      const potion = potions[mid];
+      if (potion >= target) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+
+    return right;
+  });
 };
+
+console.log(successfulPairs([5, 1, 3], [1, 2, 3, 4, 5], 7));
