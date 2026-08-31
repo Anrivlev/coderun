c15fbf3ee7a0a0eab27e8ab37f38ca37ad3084a9
anrivlev <ivlev@itgeo.ru>
2026-08-31 15:20:49 +0300

    162.FindPeakElement.js

---
+++ leetcode/leetcode-75/binary-search/162.FindPeakElement.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+    if (nums.length === 1) return 0;
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] < nums[mid + 1]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return left;
+};
+
+console.log(findPeakElement([1, 2, 3, 1]));
+console.log(findPeakElement([1, 2]));
