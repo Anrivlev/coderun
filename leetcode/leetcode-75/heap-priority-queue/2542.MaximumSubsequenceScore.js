bdf03f35ff83f8197cb1f675f2b547f917063a2b
anrivlev <ivlev@itgeo.ru>
2026-08-31 12:27:23 +0300

    2542.MaximumSubsequenceScore.js Сложная задача, я не уверен, что смогу воспроизвести решение. Попробую еще раз в будущем.

--- leetcode/leetcode-75/heap-priority-queue/2542.MaximumSubsequenceScore.js
+++ leetcode/leetcode-75/heap-priority-queue/2542.MaximumSubsequenceScore.js
@@ -4,6 +4,23 @@
  * @param {number} k
  * @return {number}
  */
-var maxScore = function(nums1, nums2, k) {
-    
+var maxScore = function (nums1, nums2, k) {
+  const merged = nums1.map((num1, index) => [num1, nums2[index]]);
+  merged.sort((a, b) => b[1] - a[1]);
+
+  const heap = new MinPriorityQueue();
+  let max = 0;
+  let sum = 0;
+
+  for (const [num1, num2] of merged) {
+    sum+= num1;
+    heap.enqueue(num1);
+
+    if (heap.size() === k) {
+        max = Math.max(max, sum * num2);
+        sum -= heap.dequeue();
+    }
+  }
+
+  return max;
 };
