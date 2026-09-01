ecc0d33ae7d21af2ac0f646e6f23005edbf3beb7
anrivlev <ivlev@itgeo.ru>
2026-09-01 10:24:41 +0300

    746.MinCostClimbingStairs.js

--- leetcode/leetcode-75/dp-1d/746.MinCostClimbingStairs.js
+++ leetcode/leetcode-75/dp-1d/746.MinCostClimbingStairs.js
@@ -2,6 +2,16 @@
  * @param {number[]} cost
  * @return {number}
  */
-var minCostClimbingStairs = function(cost) {
-    
+var minCostClimbingStairs = function (cost) {
+  if (cost.length === 0) return 0;
+  if (cost.length === 1) return cost[0];
+
+  let prevPrevMin = cost[0];
+  let prevMin = cost[1];
+  for (let i = 2; i < cost.length; i++) {
+    const nextMin = Math.min(prevPrevMin, prevMin) + cost[i];
+    prevPrevMin = prevMin;
+    prevMin = nextMin;
+  }
+  return Math.min(prevPrevMin, prevMin);
 };
