07298b4ea212a2e5f11f3d69f4bbe9f435ebc93b
anrivlev <ivlev@itgeo.ru>
2026-09-01 16:32:11 +0300

    208.ImplementTrie(Prefix Tree).js

---
+++ leetcode/leetcode-75/trie/208.ImplementTrie(Prefix Tree).js
@@ -0,0 +1,56 @@
+var Trie = function () {
+  this.root = { key: null, isWord: false, children: new Map() };
+};
+
+/**
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function (word) {
+  let currentNode = this.root;
+  for (const token of word) {
+    let nextNode = currentNode.children.get(token);
+    if (!nextNode) {
+      nextNode = { key: token, isWord: false, children: new Map() };
+      currentNode.children.set(token, nextNode);
+    }
+    currentNode = nextNode;
+  }
+  currentNode.isWord = true;
+};
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function (word) {
+  let currentNode = this.root;
+  for (const token of word) {
+    const nextNode = currentNode.children.get(token);
+    if (!nextNode) return false;
+    currentNode = nextNode;
+  }
+  return currentNode.isWord;
+};
+
+/**
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function (prefix) {
+  let currentNode = this.root;
+  for (const token of prefix) {
+    const nextNode = currentNode.children.get(token);
+    if (!nextNode) return false;
+    currentNode = nextNode;
+  }
+  return true;
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
