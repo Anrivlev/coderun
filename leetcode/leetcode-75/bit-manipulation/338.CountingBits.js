c83ae25898207004acf1d0f5e2c3715c97f0840a
anrivlev <ivlev@itgeo.ru>
2026-09-02 13:00:44 +0300

    refactored to make solution clearer to read

--- leetcode/leetcode-75/bit-manipulation/338.CountingBits.js
+++ leetcode/leetcode-75/bit-manipulation/338.CountingBits.js
@@ -5,13 +5,16 @@
 var countBits = function (n) {
   const answer = new Array(n + 1).fill(0);
   for (let i = 1; i < answer.length; i++) {
-    let number = i;
-    let count = 0;
-    while (number > 0) {
-      count += number % 2;
-      number = Math.floor(number / 2);
-    }
-    answer[i] = count;
+    answer[i] = getNumberOfOnes(i);
   }
   return answer;
 };
+
+function getNumberOfOnes(number) {
+  let count = 0;
+  while (number > 0) {
+    count += number % 2;
+    number = Math.floor(number / 2);
+  }
+  return count;
+}
