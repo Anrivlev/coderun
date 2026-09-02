f101c6c1c8f69123ffe80d9c21540b3a729d36af
anrivlev <ivlev@itgeo.ru>
2026-09-02 12:56:45 +0300

    338.CountingBits.js solution from me (not optimal, n ln n)

--- leetcode/leetcode-75/bit-manipulation/338.CountingBits.js
+++ leetcode/leetcode-75/bit-manipulation/338.CountingBits.js
@@ -2,4 +2,16 @@
  * @param {number} n
  * @return {number[]}
  */
-var countBits = function (n) {};
+var countBits = function (n) {
+  const answer = new Array(n + 1).fill(0);
+  for (let i = 1; i < answer.length; i++) {
+    let number = i;
+    let count = 0;
+    while (number > 0) {
+      count += number % 2;
+      number = Math.floor(number / 2);
+    }
+    answer[i] = count;
+  }
+  return answer;
+};
